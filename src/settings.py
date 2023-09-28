918f4997a7dbbbf649c99ba104e3a64fd0c7fb2e
erastov-alex <erastov.alex@gmail.com>
2023-09-28 14:56:31 +0300

    set preview

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/usmanafzaal/strawberry-disease-detection-dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 5243553
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/strawberry-disease-detection"
